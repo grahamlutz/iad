493c85a67e5f4a12f63b9d10276271b1256a82fc
Graham Lutz <graham@brainbytescreative.com>
2017-02-02 10:18:37 -0500

    adds custom post type dog and sets default user-role to editor

--- wp-content/themes/itsadog/functions.php
+++ wp-content/themes/itsadog/functions.php
@@ -148,3 +148,41 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Dog Custom Post Type
+ */
+
+ function custom_post_dog() {
+   $labels = array(
+     'name'               => _x( 'Dogs', 'post type general name' ),
+     'singular_name'      => _x( 'Dog', 'post type singular name' ),
+     'add_new'            => _x( 'Add New', 'book' ),
+     'add_new_item'       => __( 'Add New Dog' ),
+     'edit_item'          => __( 'Edit Dog' ),
+     'new_item'           => __( 'New Dog' ),
+     'all_items'          => __( 'All Dogs' ),
+     'view_item'          => __( 'View Dog' ),
+     'search_items'       => __( 'Search Dogs' ),
+     'not_found'          => __( 'No dogs found' ),
+     'not_found_in_trash' => __( 'No dogs found in the Trash' ),
+     'parent_item_colon'  => '',
+     'menu_name'          => 'Dogs'
+   );
+   $args = array(
+     'labels'        => $labels,
+     'description'   => 'Holds our dogs and dog specific data',
+     'public'        => true,
+     'menu_position' => 5,
+     'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt', 'comments' ),
+     'has_archive'   => false,
+   );
+   register_post_type( 'dog', $args );
+ }
+ add_action( 'init', 'custom_post_dog' );
+
+ /**
+  * Set Default User Role
+  */
+
+update_option('default_role','editor');
