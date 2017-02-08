a56e1b7fc141784f17b3ac2bbd214b2c42017db9
Graham Lutz <graham@brainbytescreative.com>
2017-02-08 12:34:22 -0500

    adds basical gallery.php page template

---
+++ wp-content/themes/itsadog/page-templates/gallery.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: Gallery
+ * Description: Page template without sidebar
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package itsadog
+ */
+
+get_header();
+
+?>
