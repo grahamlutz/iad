bb7c2315f97c482684516bd113c05430f4708b92
Graham Lutz <graham@brainbytescreative.com>
2017-02-28 14:27:01 -0500

    typo

---
+++ wp-content/themes/itsadog/page-templates/edit-account-info.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: Edit Account Info
+ * Description: Page template without sidebar
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package itsadog
+ */
+
+get_header();
+
+?>
+
+<div id="primary" class="content-area">
+	<main id="edit-account-info" class="site-main" role="main">
+
+	<?php
+
+	echo do_shortcode( '[rp_profile_edit] ' );
+
+	?>
+	
+	</main>
+</div> <!-- #primary -->
