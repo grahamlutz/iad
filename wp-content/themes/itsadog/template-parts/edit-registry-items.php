2e2d74b0a423770724e7d8d08460bfad9f2ee54a
Graham Lutz <graham@brainbytescreative.com>
2017-03-07 13:34:34 -0500

    edit edit-registry-items.php template to use edit registry button and modal

--- wp-content/themes/itsadog/template-parts/edit-registry-items.php
+++ wp-content/themes/itsadog/template-parts/edit-registry-items.php
@@ -9,102 +9,161 @@
 
 ?>
 
-<div class="col-md-7 registry-items-box">
-	<?php 
+<!-- Begin Edit Registry Modal -->
 
-	$post_id = get_the_ID();
+<button type="button" class="btn btn-primary" data-post-id="<?php echo $post_id ?>" data-toggle="modal" data-target="#<?php echo $post_id ?>Modal">
+  Edit Registry
+</button>
 
-	$args = array( 
-        'post_type' => 'registry_product', 
-        'taxonomy'  => 'registry_product_categories', 
-        'orderby'   => 'id', 
-        'order'     => 'ASC' 
-    );
 
-	$categories = get_categories( $args );
-
-	foreach ($categories as $category) {
-
-		$category_name = strtolower(str_replace(" ", "_", $category->name));
-		?>
-
-		<div class="col-md-3 product_category <?php echo $category_name; ?>">
-			<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".<?php echo $post_id . $category_name; ?>Modal">
-			  <?php echo $category->name; // insert appropriate image here ?>
-			</button>
+<!-- Registry Modal -->
+<div class="modal fade" id="<?php echo $post_id ?>Modal" tabindex="-1" role="dialog" aria-labelledby="<?php echo $post_id ?>ModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        
+		<!-- Category tabs -->
+		<div class="row">
+		    <div class="col-md-3">
+			
 			<?php
-			//if ($category_name == )
+
+				$args = array( 
+		            'post_type' => 'registry_product', 
+		            'taxonomy'  => 'registry_product_categories', 
+		            'orderby'   => 'id', 
+		            'order'     => 'ASC' 
+		        );
+
+				$categories = get_categories( $args );
+
+				?>
+
+				<ul class="nav nav-tabs md-pills pills-primary flex-column" role="tablist">
+
+				<?php
+
+				foreach ($categories as $category) {
+
+					$category_name = strtolower(str_replace(" ", "_", $category->name));
+
+					?>
+
+					<li class="nav-item">
+		                <a class="nav-link active" data-category="<?php echo $category_name; ?>" data-toggle="tab" href="#panel<?php echo $category_name; ?>" role="tab"><?php echo $category->name; ?></a>
+		            </li>
+
+					<?php
+				};
 			?>
-		</div>
 
-		<!-- Modal to display all items in seleced category -->
-		<div class="modal fade <?php echo $post_id . $category_name; ?>Modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-		  <div class="modal-dialog modal-lg">
-		    <div class="modal-content">
-		      <?php
-
-		      	$product_args = array(
-	                            'post_type'     => 'registry_product',
-	                            'orderby'       => 'id',
-	                            'order'         => 'ASC',
-	                            'post_status'   => 'publish',
-	                            'tax_query'     => array(
-													 array(
-													  	'taxonomy' => 'registry_product_categories',
-														'field'    => 'slug',
-														'terms'    => $category->slug,
-													),
-												),
-	                        );
-
-	            $product_list = new WP_Query ( $product_args ); 
-
-	            ?>
-
-	            <?php while ( $product_list -> have_posts() ) : $product_list -> the_post();
-
-	            	 $img_url = get_post_meta( get_the_ID(), 'item_image_url', true );
-	            	 $asin_code = get_post_meta( get_the_ID(), 'asin_code', true ); 
-	            	 $current_registry_product  = get_posts(array(
-						'post_type' => 'dog',
-						'meta_query' => array(
-							array(
-								'key' => $category->slug, // name of custom field
-								'value' => '"' . get_the_ID() . '"', // matches exaclty "123", not just 123. This prevents a match for "1234"
-								'compare' => 'LIKE'
-							)
-						),
-						'author' => get_current_user_id()
-					));
-	            ?>
-
-	                <div class="product">
-	                	<h3 class="title ellipsis"><?php the_title(); ?></h3>
-	                    <a class="asin-code <?php echo $asin_code ?>" data-category="<?php echo $category->slug ?>" data-asin-code="<?php echo $asin_code ?>" href="">
-	                    	<img src="<?php echo $img_url ?>" alt="">
-	                    </a>
-	                    <?php
-	              //       if ($current_registry_product) {
-	              //       	?>
-	                         <!-- <p>this is in dog's regisry</p> -->
-	                    	<?php
-	              //       }
-	              //       echo "<pre class='tada'>";
-		            	// var_dump($current_registry_product);
-		            	// echo "</pre>";
-	                
-	                    ?>
-	                </div>
-
-	            <?php endwhile; wp_reset_query(); 
-
-		      ?>
+		        </ul>
 		    </div>
-		  </div> <!-- .modal-dialog -->
-		</div> <!-- end .<?php echo $post_id . $category_name; ?>Modal -->
+		    <div class="col-md-7">
+		        <!-- Tab panels -->
+		        <div class="tab-content vertical">
+		        	<?php
 
-		<?php
-	}
+		        	foreach ($categories as $category) {
 
-	?>
-</div> <!-- end .registry-items-box -->
+						$category_name = strtolower(str_replace(" ", "_", $category->name));
+
+						?>
+
+						<div class="<?php echo $category_name; ?> tab-pane fade <?php if ($category_name == 'beds_and_blankets'){ echo "in active";} ?>" id="panel<?php echo $category_name; ?>" role="tabpanel" data-category="<?php echo $category_name; ?>">
+			                <br>
+			                <?php
+
+					      	$product_args = array(
+				                            'post_type'     => 'registry_product',
+				                            'orderby'       => 'id',
+				                            'order'         => 'ASC',
+				                            'post_status'   => 'publish',
+				                            'tax_query'     => array(
+																 array(
+																  	'taxonomy' => 'registry_product_categories',
+																	'field'    => 'slug',
+																	'terms'    => $category->slug,
+																),
+															),
+				                        );
+
+				            $product_list = new WP_Query ( $product_args ); 
+				            
+				            $product_IDs = getCurrentRegistryProductIDs( $category_name, $post_id );
+			            	
+				            ?>
+							<div class="col-md-6 in-registry">
+								<h3>In Registry</h3>
+					            <?php while ( $product_list -> have_posts() ) : $product_list -> the_post();
+
+					            	$img_url = get_post_meta( get_the_ID(), 'item_image_url', true );
+					            	$asin_code = get_post_meta( get_the_ID(), 'asin_code', true ); 
+
+					            	$item_key = array_search( get_the_ID(), $product_IDs );
+					            	$item_key++;
+									
+									if ( $item_key ) {
+									?>
+										<div class="product <?php echo $category->slug ?>" id="product<?php echo get_the_ID() ?>">
+						                	<h3 class="title ellipsis"><?php the_title(); ?></h3>
+						                    <a class="asin-code <?php echo $asin_code ?>" data-category="<?php echo $category->slug ?>" data-asin-code="<?php echo $asin_code ?>" href="">
+						                    	<img src="<?php echo $img_url ?>" alt="">
+						                    </a>
+						                </div>
+						            <?php
+									}
+									?>
+					            <?php endwhile; wp_reset_query(); 
+					            ?>
+					        </div> <!-- .in-registry -->
+					        <div class="col-md-6 not-in-registry">
+					        	<h3>Not in Registry</h3>
+					            <?php while ( $product_list -> have_posts() ) : $product_list -> the_post();
+
+					            	$img_url = get_post_meta( get_the_ID(), 'item_image_url', true );
+					            	$asin_code = get_post_meta( get_the_ID(), 'asin_code', true ); 
+
+					            	$item_key = array_search( get_the_ID(), $product_IDs );
+					            	$item_key++;
+					            ?>
+									<?php
+										if ( $item_key === false ) {
+										?>
+											<div class="product <?php echo $category->slug ?>" id="product<?php echo get_the_ID() ?>">
+							                	<h3 class="title ellipsis"><?php the_title(); ?></h3>
+							                    <a class="asin-code <?php echo $asin_code ?>" data-category="<?php echo $category->slug ?>" data-asin-code="<?php echo $asin_code ?>" href="">
+							                    	<img src="<?php echo $img_url ?>" alt="">
+							                    </a>
+							                </div>
+							            <?php
+										}
+									?>
+					            <?php endwhile; wp_reset_query(); 
+					            ?>
+					        </div> <!-- .in-registry -->
+			            </div> <!-- .tab-pane -->
+
+						<?php
+					};
+
+					?>
+		 
+		        </div> <!-- .tab-content -->
+		    </div>
+		</div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div> <!-- .modal -->
+
+<!-- End Edit Registry Modal -->
